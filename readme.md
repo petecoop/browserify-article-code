f324ce29e4b3fd764e73e02ae608aa15ffffe518
petecoop <pete@petecoop.co.uk>
2014-01-19 18:36:58 +0000

    added readme and weather app

---
+++ readme.md
@@ -0,0 +1,3 @@
+#Modular JavaScript with Browserify
+
+Code for my article on browserify: http://petecoop.co.uk/blog/modular-javascript-with-browserify
---
+++ weather-app/modules/weather.js
@@ -0,0 +1,7 @@
+var jsonp = require('./jsonp');
+
+var url = 'http://api.openweathermap.org/data/2.5/weather?units=metric&callback=jsonpCallback&q=';
+
+exports.get = function (city, done) {
+  jsonp(url + city, done);
+};
